bd56564c3157c45d75b2b18b30fdf8d2a611557a
matt0 <matt@planet-labs.com>
2014-02-06 01:04:20 -0800

    RAILS LOUNGE: initial commit, runs locally!

---
+++ site/rails/lounge/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lounge::Application.config.secret_key_base = '41772d9fed249b0cd87530d23530fbd41154039c44e8825ff1812172ff433818624cec3174cac0f27578c935095aea37aa3fa05062816c1423d90347f58e7ea5'
---
+++ site/rails/lounge/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lounge::Application.config.session_store :cookie_store, key: '_lounge_session'
